cdbfc0a61f8442e4b04cd4fe2fde1449b2f79937
PiaSR <pia@richling.at>
2025-09-10 14:24:37 +0200

    connected sanity

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,17 +1,24 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
-
 import react from '@astrojs/react';
-
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
   image: {
       service: {
-        entrypoint: 'astro/assets/services/sharp' // Default in Astro 5
+        entrypoint: 'astro/assets/services/sharp'
       }
-	  },
+    },
 
-  integrations: [react()]
+  integrations: [
+    react(),
+    sanity({
+      // Your Sanity Project ID and Dataset
+      projectId: '6uu4aiks', 
+      dataset: 'production', 
+      useCdn: true,
+      apiVersion: '2025-09-10' // Use today's date for a consistent API version
+    }),
+  ]
 });
