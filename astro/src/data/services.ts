033b11e939249b620d6d7e7658a14829864b44a3
PiaSR <pia@richling.at>
2025-09-20 12:55:08 +0200

    finalised about page components

---
+++ astro/src/data/services.ts
@@ -0,0 +1,51 @@
+export const servicesData = [
+	{
+	  title: "Mietzinshäuser (Zinshausverwaltung)",
+	  description:
+		"Bei Mietzinshäusern steht neben der technischen Betreuung vor allem die wirtschaftliche Optimierung im Mittelpunkt. Wir übernehmen die vollständige kaufmännische und mietrechtliche Verwaltung und sorgen für einen reibungslosen Ablauf im laufenden Betrieb. Je nach Ihren Wünschen stellen wir Ihnen unser Leistungspaket zusammen.",
+	  image: "images/Zinshaus-img.jpg",
+	  alt: "Zinshaus",
+	  listItems: [
+		"Verwaltung der Mieteinnahmen und Betriebskosten",
+		"Ausarbeitung und Abschluss von Mietverträgen",
+		"Indexanpassungen und Mietzinsüberprüfungen",
+		"Mietvorschreibung, Mahnwesen und Kontrolle der Zahlungseingänge",
+		"Betreuung von Mieteranfragen und Schadensmeldungen",
+		"Ausschreibung und Überwachung von Instandhaltungs- und Sanierungsarbeiten",
+		"Organisation und Beauftragung von Sanierungsmaßnahmen in den Mietobjekten",
+		"Laufende Abstimmung mit der Eigentümerin bzw. dem Eigentümer",
+	  ],
+	},
+	{
+	  title: "Wohnungseigentums- gemeinschaften (WEG-Verwaltung)",
+	  description:
+		"Im Rahmen der Verwaltung von Wohnungseigentumsgemeinschaften übernehmen wir die kaufmännische, technische und organisatorische Betreuung der gesamten Liegenschaft. Grundlage ist das Wohnungseigentumsgesetz (WEG).",
+	  image: "images/WEG-image.jpg",
+	  alt: "WEG-Verwaltung",
+	  listItems: [
+		"Erstellung der jährlichen Abrechnung (Betriebskosten und Rücklage) gemäß § 34 WEG",
+		"Vorschreibung und Anpassung der monatlichen Zahlungen (Akontierung)",
+		"Laufende Buchhaltung und Zahlungsverkehr",
+		"Einberufung, Organisation und Protokollierung der Eigentümerversammlung",
+		"Umsetzung von Beschlüssen der Eigentümergemeinschaft",
+		"Beauftragung und Koordination von Instandhaltungsmaßnahmen",
+		"Kommunikation mit Professionisten, Behörden und Eigentümer:innen",
+		"Prüfung von Versicherungsschäden und Schadensabwicklung",
+	  ],
+	},
+	{
+	  title: "Subverwaltung einzelner Einheiten",
+	  description:
+		"Wir übernehmen auch die Verwaltung einzelner Eigentumswohnungen innerhalb größerer Liegenschaften - z. B. für vermietete Wohnungen, deren Eigentümer:innen nicht selbst vor Ort sind oder keine laufende Betreuung übernehmen möchten.",
+	  image: "images/subverwaltung-image.jpg",
+	  alt: "Subverwaltung",
+	  listItems: [
+		"Mietvorschreibung und Abrechnung der Betriebskosten",
+		"Mietverträge, Indexierung, Kommunikation mit Mieter:innen",
+		"Organisation kleinerer Reparaturen oder Instandhaltungen",
+		"Ansprechpartnerfunktion für Mieter:innen bei Fragen zur Wohnung",
+		"Prüfung der Hausabrechnung im Sinne des Eigentümers / der Eigentümerin",
+		"Optionale Unterstützung bei Mieterwechsel (inkl. Übergabe, Rücknahme, Vermittlung)",
+	  ],
+	},
+  ];
