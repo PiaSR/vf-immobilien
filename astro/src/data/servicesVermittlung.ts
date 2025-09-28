5ae7c918df93f4d069ec26ad94280b815d789562
PiaSR <pia@richling.at>
2025-09-28 19:20:56 +0200

    added services page complete

---
+++ astro/src/data/servicesVermittlung.ts
@@ -0,0 +1,23 @@
+import VermittlungImage from "../assets/images/Vermittlung.jpg";
+
+
+export const servicesVermittlungData = [
+	{
+	  title: "Immobilien&shy;vermittlung für Miete und Kauf",
+	  description:
+		"Verlässlich, diskret und gut vorbereitet. Neben der laufenden Verwaltung bieten wir umfassende Leistungen im Bereich der Immobilienvermittlung. Ob Verkauf oder Vermietung – wir begleiten Eigentümer:innen von der Erstberatung bis zur erfolgreichen Übergabe und sorgen für eine rechtlich fundierte und geordnete Abwicklung.",
+	  image: VermittlungImage,
+	  alt: "Black old wrought-iron gate",
+	  listItems: [
+		"Vermittlung von Eigentumswohnungen, Ein- und Mehrfamilienhäuser, und Geschäftslokale",
+		"Persönliche Erstberatung und Objektaufnahme",
+		"Markteinschätzung / Mietzinsanalyse",
+		"Aufbereitung aller relevanten Unterlagen (Pläne, Grundbuch, Energieausweis etc.)",
+		"Erstellung eines Exposés mit aussagekräftigen Fotos",
+		"Bewerbung über relevante Kanäle und persönliche Netzwerke",
+		"Organisation und Durchführung von Besichtigungen",
+		"Ausarbeitung und Abschluss von Mietverträgen",
+	  ],
+	},
+	
+  ];
