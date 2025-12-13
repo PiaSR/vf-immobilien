0199810ee20784221f366f68a24bbc45f46893d5
PiaSR <pia@richling.at>
2025-12-13 14:45:47 +0200

    redeploying after clearing caches

--- astro/.vercel/output/_functions/chunks/Open-in-new-window_CLAK6KXV.mjs
+++
@@ -1,7 +0,0 @@
-import { c as createSvgComponent } from './MainLayout_DU7Rr5R3.mjs';
-
-const docIcon = createSvgComponent({"meta":{"src":"/_astro/Document-icon-svg.DJeBoiQf.svg","width":24,"height":24,"format":"svg"},"attributes":{"height":"24px","viewBox":"0 -960 960 960","width":"24px","fill":"currentColor"},"children":"<path d=\"M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z\" />"});
-
-const openInNewWindow = createSvgComponent({"meta":{"src":"/_astro/Open-in-new-window.BPRVG6Gk.svg","width":24,"height":24,"format":"svg"},"attributes":{"height":"24px","viewBox":"0 -960 960 960","width":"24px","fill":"currentColor"},"children":"<path d=\"M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z\" />"});
-
-export { docIcon as d, openInNewWindow as o };
