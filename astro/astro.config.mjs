670fe0501bb9740d5f8ffa9f75848316b64595db
PiaSR <pia@richling.at>
2025-12-16 10:09:55 +0200

    sitemap and robots txt

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
- 
+  site: 'https://vf-immobilien.vercel.app',
   
   integrations: [
     react(),
+    sitemap(),
     sanity({
       // Your Sanity Project ID and Dataset
       projectId: '6uu4aiks', 
